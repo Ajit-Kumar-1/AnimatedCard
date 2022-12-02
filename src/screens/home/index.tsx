d50ae64256d4d0c7ea1323bec5bf40b733dd95c2
AjitKumar-1 <ajitkumarxc8@gmail.com>
2022-12-02 19:13:52 +0000

    Updated home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import {View, StyleSheet, Text, SafeAreaView} from 'react-native';
 import {CONTAINER} from '../../utils/Styles';
-import {HEREWORKS} from '../../utils/Strings';
+import {HEREWORKS, HERE_ARE_YOUR_LATEST_UPDATES} from '../../utils/Strings';
 import Card from './components/Card';
-import {CEMENT} from '../../utils/Colors';
+import {CEMENT, WHITE} from '../../utils/Colors';
 
 const HomeScreen = () => (
-  <SafeAreaView style={CONTAINER}>
-    <Text style={styles.title}>{HEREWORKS}</Text>
-    <Card weekData={[64, 47, 55, 62, 60, 64, 62]} />
-  </SafeAreaView>
+  <View style={CONTAINER}>
+    <View style={styles.mainSection}>
+      <SafeAreaView>
+        <Text style={styles.title}>{HEREWORKS}</Text>
+        <View style={styles.welcomeSection}>
+          <Text style={styles.greeting}>Hi Derek 👋</Text>
+          <Text style={styles.latestUpdatesText}>
+            {HERE_ARE_YOUR_LATEST_UPDATES}
+          </Text>
+        </View>
+        <Card weekData={[64, 47, 55, 62, 60, 64, 62]} />
+      </SafeAreaView>
+    </View>
+  </View>
 );
 
 const styles = StyleSheet.create({
@@ -17,9 +27,27 @@ const styles = StyleSheet.create({
     color: CEMENT,
     marginStart: 24,
     marginTop: 19,
+    marginBottom: 26,
     fontWeight: '900',
     fontSize: 28,
     lineHeight: 36,
+  },
+  mainSection: {
+    backgroundColor: WHITE,
+  },
+  welcomeSection: {
+    marginHorizontal: 24,
+  },
+  greeting: {
+    fontSize: 22,
+    lineHeight: 30,
+    fontWeight: '900',
+    color: CEMENT,
+  },
+  latestUpdatesText: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: '400',
   },
 });
 
--- src/utils/Strings.ts
+++ src/utils/Strings.ts
@@ -1,7 +1,9 @@
 export const BOOKING = 'Booking';
 export const FIND_OUT_WHY = 'Find out why';
 export const HAPPY_SCORE = 'Happy score';
+export const HERE_ARE_YOUR_LATEST_UPDATES = 'Here are your latest updates';
 export const HEREWORKS = 'Hereworks';
+export const HI = 'Hi';
 export const HOME = 'Home';
 export const LIVE_UPPERCASE = 'LIVE';
 export const MY_BOOKINGS = 'My bookings';
