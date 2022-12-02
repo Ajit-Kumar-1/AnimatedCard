c110c3954394b522b82194aab5fd088cef054906
AjitKumar-1 <ajitkumarxc8@gmail.com>
2022-12-02 23:42:23 +0000

    Added shadow color

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -7,4 +7,5 @@ export const LIGHT_BLUE = '#2173C4';
 export const LIGHT_GRAY = '#EEEDE8';
 export const PINK = '#D6006D';
 export const SCREEN_BACKGROUND = '#E5E5E5';
+export const SHADOW = '#323247';
 export const WHITE = '#FFFFFF';
