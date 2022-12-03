3d773ff224274562b3515d7169d69eb289747e7a
AjitKumar-1 <ajitkumarxc8@gmail.com>
2022-12-03 11:04:14 +0000

    Working on common text component

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import {Text, StyleSheet, FlexStyle} from 'react-native';
+import {Text, StyleSheet, FlexStyle, Platform} from 'react-native';
+import {CEMENT} from '../utils/Colors';
+
+const isIOS = Platform.OS === 'ios';
 
 interface LabelProps {
   children: string;
   style?: FlexStyle;
+  bold?: boolean;
 }
 
 const Label = (props: LabelProps) => (
-  <Text style={props.style}>{props.children}</Text>
+  <Text style={[props.bold ? styles.bold : styles.regular, props.style]}>
+    {props.children}
+  </Text>
 );
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  bold: {
+    fontFamily: isIOS ? 'Roboto' : 'Roboto-Bold',
+    color: CEMENT,
+  },
+  regular: {
+    fontFamily: isIOS ? 'Roboto' : 'Roboto-Regular',
+    color: CEMENT,
+    fontSize: 12,
+    lineHeight: 20,
+  },
+});
 
 export default Label;
