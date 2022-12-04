fd25a5e9e3f87ec666aa502cf8c7c71b30df10d8
AjitKumar-1 <ajitkumarxc8@gmail.com>
2022-12-04 15:39:04 +0000

    Changed screen background color

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -9,6 +9,6 @@ export const LIGHT_BLUE = '#2173C4';
 export const LIGHT_GRAY = '#EEEDE8';
 export const PINK = '#D6006D';
 export const PRIMARY_GRAY = '#D9D8D6';
-export const SCREEN_BACKGROUND = '#E5E5E5';
+export const SCREEN_BACKGROUND = '#FBFBFB';
 export const SHADOW = '#323247';
 export const WHITE = '#FFFFFF';
